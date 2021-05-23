b8507e62c161f45f38b6bd626111386a3a07b234
Pisitchai <pisitchai.rueangwatanaphong@gmail.com>
2021-05-23 14:22:00 +0700

    feat: 🎸 update

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App container">
+        <h1>Employee Information</h1>
+        <div className="information">
+          <form action="">
+          
+          </form>
+        </div>
+    </div>
+  );
+}
+
+export default App;
